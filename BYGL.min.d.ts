66a23e969e6cb80c6bb5eecfe68be84f770b7bb6
badyoo <547243998@qq.com>
2020-01-04 12:53:40 +0800

    代码提示.d.ts

---
+++ BYGL.min.d.ts
@@ -0,0 +1,28 @@
+export = BadYoo;
+export as namespace BadYoo;
+declare namespace BadYoo 
+{
+    function Instantiate(o:GameObject):GameObject
+    function power(width:number = 0,height:number = 0,alignC:boolean = true,canvas:HTMLCanvasElement = null,root = null):void
+    function bgColor(color:number);
+    class GameObject
+    {
+        x:number;
+        y:number;
+        pivotX:number;
+        pivotY:number;
+        scaleX:number;
+        scaleY:number;
+        rotation:number;
+        matrix:any
+        pivot(x:number,y:number):void;
+        scale(x:number,y:number):void;
+        toParentPoint(point:Point):Point;
+        fromParentPoint(point:Point):Point;
+        localToGlobal(point:Point,newPoint:boolean = false,g:GameObject = null):Point;
+        globalToLocal(point:Point,newPoint:boolean = false,g:GameObject = null):Point;
+        hitTest(point:Point):boolean;
+        move(x:number,y:number):void;
+        free():void;
+    }
+}
